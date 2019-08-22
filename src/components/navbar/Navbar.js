31d5588ac22d129ceaebe93b561268570c0d3547
april <martinezapril56@yahoo.com>
2019-08-22 18:58:22 -0400

    navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Navbar.min.css';
+
+
+ class Navbar extends Component {
+  render() {
+    return (
+       <div>
+          <nav class="nav-center">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+      </nav>
+       </div>
+    
+    );
+  }
+}
+
+
+export default Navbar;
