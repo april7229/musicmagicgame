eb35e9f53a5ee837175c3aa0ff5f516b44e18fd8
april <martinezapril56@yahoo.com>
2019-08-25 22:55:19 -0400

    april

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react'
 
 
 
- class Main extends Component {
+class Main extends Component {
     render() {
         return (
-           <>  
-           <section id="projects">
-  <h1>For you chris</h1>
-  <div class="grid-container">
-       <div class="row">
-               <div class="For you chris">
-          <iframe  src="https://www.youtube.com/embed/DyDfgMOUjCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                 </div>   
-               
-    </div>
-    
-                 
-                             </div>
-                                  
-         
-</section>
+            <>
+                <section id="projects">
+                    <h1>For you chris</h1>
+                    <div class="grid-container">
+                        <div class="row">
+                            <div class="For you chris">
+                                <iframe src="https://www.youtube.com/embed/DyDfgMOUjCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                </section>
+                <section id="projects">
+                    <h1>For you shawn</h1>
+                    <div class="grid-container">
+                        <div class="row">
+                            <div class="For you chris">
+                            <iframe src="https://www.youtube.com/embed/PBwAxmrE194" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                </section>
             </>
-            
+
         )
     }
 }
