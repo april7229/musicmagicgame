b75fb932e139a889627a59fd6d330a5f5140d859
april <martinezapril56@yahoo.com>
2019-08-25 22:30:10 -0400

    chris

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import Main from './components/Main/Main'
 import Navbar from "./components/navbar/Navbar";
 import Index from "./layout/Index";
 import Lyrics from "./Tracks/Lyrics";
@@ -14,11 +14,12 @@ const App = () => {
     <ContextController>
       <Router>
         <>
-          {/* <Navbar /> */}
+          <Navbar />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
               <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              <Route exact path="/Main" component={Main} />
             </Switch>
           </div>
         </>
