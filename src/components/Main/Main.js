713cfcb87e6bef18539a612d49d865e077258fdd
april <martinezapril56@yahoo.com>
2019-08-26 23:28:57 -0400

    aprl

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,33 +7,28 @@ class Main extends Component {
     render() {
         return (
             <>
-                <section id="projects">
-                    <div class="grid-container">
-                        <div class="grid-item"> 1                           
-                                <iframe src="https://www.youtube.com/embed/DyDfgMOUjCI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                    </div>
-                </section>
-                <section id="projects">
+         
 
-                    <div class="grid-container">
-                        <div class="grid-item"> 2                    
-                                <iframe src="https://www.youtube.com/embed/PBwAxmrE194" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                            </div>
-                        </div>                </section>
-                <section id="projects">
-                   <div class="grid-container">
-                        <div class="grid-item">3
+                
+<h1>Music </h1>
 
-                            <iframe git src="https://www.youtube.com/embed/XhzpxjuwZy0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
+<p>Music can change your life.</p>
 
+<div class="grid-container">
+  <div>
+  <iframe git src="https://www.youtube.com/embed/XhzpxjuwZy0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+  <div><iframe src="https://www.youtube.com/embed/Cc7M0i78wrU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div> <iframe src="https://www.youtube.com/embed/w_ufWTeabHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>  
+  <div><iframe  src="https://www.youtube.com/embed/ba7mB8oueCY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div><iframe  src="https://www.youtube.com/embed/zM0KAh5w7Rs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div><iframe  src="https://www.youtube.com/embed/eEJ2b6IaGWU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div><iframe  src="https://www.youtube.com/embed/cjIvu7e6Wq8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <div><iframe src="https://www.youtube.com/embed/L_u97PqWX6g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+</div>
 
+<p>music makes you feel things you can not explain</p>
 
-
-
-                </section>
                 
             </>
 
